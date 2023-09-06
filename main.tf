1617d62bee3a2863f8632b3ab7914c8377f9b4f0
badrummond <144220944+badrummond@users.noreply.github.com>
2023-09-06 12:31:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["al2023-ami-2023.0.20230322.0-kernel-6.1-x86_64"]
+    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
   }
 
   filter {
